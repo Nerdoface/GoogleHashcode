04c48e41f87703258bed1a3ffa3e940826f356de
Maiko Bergman <lolz4live@hotmail.com>
2018-03-01 21:55:07 +0100

    Car gefinished

--- Car.cpp
+++ Car.cpp
@@ -13,19 +13,36 @@ public:
 	int posX;
 	int posY;
 
-
-	void getFastestRoute(std::list<Route> routes){
+	void getFastestRoute(std::list<Route> routes, int curStep, int bonus){
 
 		
-		int shortestDistance = -1;
+		int shortestDistance = 0;
 		std::list<Route>::iterator car;
 
 
 		std::list<Route>::iterator it;
 		for (it = routes.begin(); it != routes.end(); ++it){
-			int carDistance = it->getLength() - (abs(posX - it->eX) + abs(posY - it->eY));
 
-			if (shortestDistance == -1 || carDistance < shortestDistance){
+			int score = 0;
+
+			int carDistance = (abs(posX - it->sX) + abs(posY - it->sY));
+
+			if (it->sF < carDistance + it->getLength()){
+				continue;
+			}
+			else{
+				int numSteps = it->sF - curStep;
+				if (carDistance <= numSteps){
+					score += it->getLength() + bonus - numSteps;
+				}
+				else{
+
+					score += it->getLength() - carDistance;
+
+				}
+			}
+
+			if (carDistance > shortestDistance){
 				shortestDistance = carDistance;
 				car = it;
 			}
