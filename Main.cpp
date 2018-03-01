9b0a8a9bd69423e36b90939bf8356bb399fd6382
Maiko Bergman <lolz4live@hotmail.com>
2018-03-01 20:35:45 +0100

    F

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <vector>
+#include <cmath>  
 
 
 int main(){
 
 
 	return 0;
+}
+
+int getPriority(int step, std::vector<int> route, int bonus, std::vector<std::vector<int>> cars){
+
+	int score = 0;
+
+	int distance = (std::abs(route.at(2) - route.at(0)) + std::abs(route.at(3) - route.at(1)));
+
+	score += distance;
+
+	for (int i = 0; i < cars.size(); i++){
+		int cDistance = (std::abs(cars.at(i).at(0) - route.at(0)) + std::abs(cars.at(i).at(1) - route.at(1)));
+
+		if (cDistance == (route.at(4) - step)){
+			score += bonus;
+		}
+
+	}
+
+	return score;
 }
